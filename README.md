d604fdb084e3f984573640965d9e053030be8373
cyk_company <cykstart@163.com>
2018-08-29 17:40:56 +0800

    第4章结束

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flask_web_study
 flask web book study
+
+
+### 1.flask????
+
--- runform.py
+++ runform.py
@@ -2,7 +2,7 @@
 # -*- coding=UTF-8 -*-
 
 from flask import Flask
-from flask import render_template,session,redirect,url_for
+from flask import render_template,session,redirect,url_for,flash
 from flask.ext.wtf import FlaskForm  #进入Form表单
 from flask.ext.bootstrap import Bootstrap  #一种开发木板
 
@@ -20,15 +20,17 @@ app.config['SECRET_KEY'] = 'hello'
 bootstrap = Bootstrap(app)
 
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        session["name"] = form.name.data
-        return redirect(url_for("index"))
-    return render_template('index.html', form=form, name=session.get("name"))
-
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form = form, name = session.get('name'))
 
 
 if __name__ == '__main__':
